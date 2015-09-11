a210752aa7de5cf43d133093886484e712aaef3b
Ján Kľuka <jan.kluka@fmph.uniba.sk>
2015-09-11 15:51:32 +0200

    Webstranky predmetov, templaty 2015 a OS X v update_infolists.sh
    Pridane stahovanie CSV zoznamov webstranok predmetov do update_infolists.sh
    do webpages.d/. Zoznam URL CSV zoznamov je vo webpages.sources (provizorny).
    Upravy pre spustanie pod OS X.
    process_data() pouzivaju template_2015_XX.html.

--- update_infolists.sh
+++ update_infolists.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+READLINK_CMD=readlink
+DATE_CMD=date
+if [ "$(uname)" = "Darwin" ]; then
+    if which greadlink > /dev/null && which gdate  > /dev/null; then
+        READLINK_CMD=greadlink
+        DATE_CMD=gdate
+    else
+        echo "This script needs GNU readlink and GNU date."
+        echo "Install coreutils, e.g., using http://brew.sh/"
+        exit 7
+    fi
+fi
 
 # nastavenia
 if [[ -z "$FAKULTA" ]]
@@ -6,17 +18,17 @@ then
   FAKULTA="FMFI"
 fi
 
-SCRIPTS="$(readlink -f "$(dirname $0)")"
+SCRIPTS="$($READLINK_CMD -f "$(dirname $0)")"
 TARGET_DIR="../infolist"
 
-if [ `date +%m` -gt 7 ]; then
+if [ `$DATE_CMD '+%m'` -gt 7 ]; then
     # zimny semester
-    LAST_YEAR=$(date +%Y)
-    THIS_YEAR=$(date +%Y -d "next year")
+    LAST_YEAR=$($DATE_CMD '+%Y')
+    THIS_YEAR=$($DATE_CMD '+%Y' -d "next year")
 else
     # letny semester
-    LAST_YEAR=$(date +%Y -d "last year")
-    THIS_YEAR=$(date +%Y)
+    LAST_YEAR=$($DATE_CMD '+%Y' -d "last year")
+    THIS_YEAR=$($DATE_CMD '+%Y')
 fi
 SEASON="$LAST_YEAR-$THIS_YEAR"
 URL="https://ais2.uniba.sk/repo2/repository/default/ais/informacnelisty"
@@ -40,13 +52,21 @@ download_data_py() {
 }
 
 
+download_webpages() {
+    urllist="$SCRIPTS/webpages.sources"
+    webpagesdir="$SCRIPTS/webpages.d"
+
+    wget -N -q -i "$urllist" -P "$webpagesdir"
+}
+
+
 process_data() {
     # Spracujeme stiahnute subory
-    python "$SCRIPTS/AIS_XML2HTML.py" "$SCRIPTS/$FAKULTA/xml_files_sk" "$TARGET_DIR/public/SK" "templates/template_table_sk.html";
-    python "$SCRIPTS/AIS_XML2HTML.py" --lang en "$SCRIPTS/$FAKULTA/xml_files_en" "$TARGET_DIR/public/EN" "templates/template_table_en.html";
+    python "$SCRIPTS/AIS_XML2HTML.py" --webpages "$SCRIPTS/webpages.d" "$SCRIPTS/$FAKULTA/xml_files_sk" "$TARGET_DIR/public/SK" "templates/template_2015_sk.html";
+    python "$SCRIPTS/AIS_XML2HTML.py" --lang en --webpages "$SCRIPTS/webpages.d" "$SCRIPTS/$FAKULTA/xml_files_en" "$TARGET_DIR/public/EN" "templates/template_2015_en.html";
 
-    python "$SCRIPTS/AIS_XML2HTML.py" --mode statnice "$SCRIPTS/$FAKULTA/xml_files_sk" "$TARGET_DIR/public/SK" "templates/template_statne-skusky_table_sk.html";
-    python "$SCRIPTS/AIS_XML2HTML.py" --mode statnice --lang en "$SCRIPTS/$FAKULTA/xml_files_en" "$TARGET_DIR/public/EN" "templates/template_statne-skusky_table_en.html";
+    python "$SCRIPTS/AIS_XML2HTML.py" --mode statnice --webpages "$SCRIPTS/webpages.d" "$SCRIPTS/$FAKULTA/xml_files_sk" "$TARGET_DIR/public/SK" "templates/template_2015_sk.html";
+    python "$SCRIPTS/AIS_XML2HTML.py" --mode statnice --lang en --webpages "$SCRIPTS/webpages.d" "$SCRIPTS/$FAKULTA/xml_files_en" "$TARGET_DIR/public/EN" "templates/template_2015_en.html";
 
     # statnice
 
@@ -57,6 +77,8 @@ process_data() {
 
 download_data_py sk
 download_data_py en
+
+download_webpages
 
 if [ "$1" != "--download-only" ]
 then
